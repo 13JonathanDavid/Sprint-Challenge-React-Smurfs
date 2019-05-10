c76207414d8a4071ed0d4130f5d051ea9c031691
13JonathanDavid <48808640+13JonathanDavid@users.noreply.github.com>
2019-05-10 13:58:35 -0400

    added CRUD methods added X (not working) to smurf elements for deletions.

--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -10,6 +10,7 @@ class Smurfs extends Component {
         <ul>
           {this.props.smurfs.map(smurf => {
             return (
+              <div>
               <Smurf
                 name={smurf.name}
                 id={smurf.id}
@@ -17,6 +18,7 @@ class Smurfs extends Component {
                 height={smurf.height}
                 key={smurf.id}
               />
+              <span>X</span></div>
             );
           })}
         </ul>
